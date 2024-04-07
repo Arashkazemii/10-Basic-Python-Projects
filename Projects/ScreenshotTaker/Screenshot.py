7876445d703a3c998ac0b42390701c86c282d671
ArashKazemi <kzmarsh19@gmail.com>
2024-04-07 16:08:58 +0330

    Screenshot.py Updated. Tkinter Button added

--- Projects/ScreenshotTaker/Screenshot.py
+++ Projects/ScreenshotTaker/Screenshot.py
@@ -1,6 +1,16 @@
 import pyscreenshot
+from tkinter import *
+
+root = Tk()
+root.title("ScreenshotTaker")
+
 num = 0
 def takeScreenshot():
-    num+=1
     image = pyscreenshot.grab()
-    image.save("\Screenshots Images\p%i"%num)
+    image.save("\\Screenshots Images\\screenshotname.png")
+    image.show()
+    
+button = Button(root, text = "Take Screenshot", command = takeScreenshot)
+button.grid(row=10, column=10)
+
+root.mainloop()
