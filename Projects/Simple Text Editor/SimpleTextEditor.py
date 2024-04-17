675ec618a0873164673f6ae439259517b54d1ae1
ArashKazemi <kzmarsh19@gmail.com>
2024-04-17 12:11:44 +0330

    New project Added. Simple Text Editor Project. simpleTextEditor.py

---
+++ Projects/Simple Text Editor/SimpleTextEditor.py
@@ -0,0 +1,48 @@
+from tkinter import *
+import tkinter as tk
+from tkinter import filedialog
+import random
+
+def openFile():
+    file_path = filedialog.askopenfilename()
+    if file_path:
+        with open(file_path, 'r') as file:
+            editor.delete('1.0', END)
+            editor.insert(END, file.read())
+
+def saveFile():
+    file_path = filedialog.asksaveasfilename(defaultextension='.txt')
+    if file_path:
+        with open(file_path, 'w') as file:
+            file.write(editor.get('1.0', END))
+
+def newFile():
+    editor.delete('1.0', END)
+
+
+root = tk.Tk()
+root.geometry('1280x720')
+root.configure(background='#fcf6f5')
+root.title("Text Editor")
+
+heading = tk.Label(root, text='Text Editor', font='bold, 20', bg='#fcf6f5', fg='#990011')
+heading.pack()
+
+scrollbar = tk.Scrollbar(root)
+scrollbar.pack(side='right', fill='y')
+
+editor = tk.Text(root, width=400, height=400, bg='white', font=(10), yscrollcommand=scrollbar.set)
+editor.pack(fill='both', padx=30, pady=20)
+
+scrollbar.config(command=editor.yview)
+
+saveButton = tk.Button(root, text='Save', fg='#fcf6f5', bg='#990011', command=saveFile)
+saveButton.place(x=20, y=10)
+
+openButton = tk.Button(root, text='Open File', fg='#fcf6f5', bg='#990011', command=openFile)
+openButton.place(x=67, y=10)
+
+newButton = tk.Button(root, text='New File', fg='#fcf6f5', bg='#990011', command=newFile)
+newButton.place(x=140, y=10)
+
+root.mainloop()
